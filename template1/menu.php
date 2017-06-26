b07ab8e649d8bc8425bcd77efa27337baefe866d
BuildTools <arobine.rbx@gmail.com>
2017-06-26 21:34:05 +0200

    Alex

---
+++ template1/menu.php
@@ -0,0 +1,17 @@
+
+<!-- DEBUT MENU -->
+
+<nav class="navbar navbar-default" id="navbartt" style="font-size: 150%;">
+  <div class="container-fluid">
+	<div class="navbar-header">
+	  <a class="navbar-brand" href="./index.php"><b>WantonGraph</b></a>
+	</div>
+	<ul class="nav navbar-nav">
+	  <li id="liacc"><a href="./index.php">Accueil</a></li>
+	  <li id="ligal"><a href="./galerie.php">Galerie</a></li>
+	  <li id="licon"><a href="./contact.php">Contact</a></li>
+	</ul>
+  </div>
+</nav>
+
+<!-- FIN MENU -->
